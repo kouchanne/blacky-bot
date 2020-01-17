2dd94f78a884e9c3824ae6cdfde4057bacaf81c0
KoukiOokawa <32385663+wc-okawa@users.noreply.github.com>
2020-01-17 16:31:32 +0900

    compile

--- index.js
+++ index.js
@@ -2,10 +2,18 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.webhook = (req, res) => {
     console.log("ブラッキーアプリ");
+    authentication(req);
     res.status(200).send("ブラッキーアプリ");
 };
 // create LINE SDK config from env variables
 const config = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN || '',
     channelSecret: process.env.CHANNEL_SECRET || ''
+};
+/*
+ * リクエスト元がLINE APIかどうかチェックする
+*/
+const authentication = (req) => {
+    console.log("認証チェック");
+    console.log(req);
 };
